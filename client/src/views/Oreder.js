ba34b6930f09e324a4cb7058e119dd18cb59ff4f
Winni Huang <usj0326@gmail.com>
2020-02-10 02:06:03 +0800

    add client folder

---
+++ client/src/views/Oreder.js
@@ -0,0 +1,264 @@
+import React, { useState, useEffect } from "react";
+import productService from "../services/productService";
+import { Link, useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import CreateRoundedIcon from "@material-ui/icons/CreateRounded";
+import DeleteRoundedIcon from "@material-ui/icons/DeleteRounded";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      margin: "0",
+      padding: "0"
+    },
+    "*::-webkit-scrollbar": {
+      width: "0em",
+      height: "8px"
+    },
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(155, 208, 208, .5)",
+      outline: "1px solid slategrey",
+      borderRadius: "15px"
+    }
+  },
+  root: {
+    position: "relative",
+    height: "100vh",
+    maxWidth: 600,
+    boxShadow: "1px 5px 15px 0px #DBDCE1",
+    overflow: "hidden"
+  },
+  grid: {
+    position: "relative",
+    height: "100vh",
+    maxWidth: 600
+  },
+  card: {
+    width: "100%",
+    marginTop: 10,
+    position: "relative"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    height: "100vh",
+    overflow: "scroll",
+    paddingBottom: 130
+    // marginLeft: -drawerWidth
+  },
+  orderDetail: {
+    listStyleType: "circle",
+    paddingLeft: 30,
+    fontFamily: "Microsoft JhengHei"
+  },
+  orderContent: {
+    marginTop: 15,
+    marginBottom: 25,
+    position: "relative",
+
+    "&::after": {
+      content: "''",
+      width: "100%",
+      position: "absolute",
+      bottom: "-15px",
+      border: "0.5px dashed #B8B9C3"
+    }
+  },
+  orderTitle: {
+    fontFamily: "Microsoft JhengHei"
+  },
+  featureBtn: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+
+    "& > a": {
+      "& > button": {
+        minWidth: "auto",
+        "& > span > svg": {
+          color: "#7FABAB"
+        }
+      }
+    },
+    "& > button": {
+      minWidth: "auto",
+      lineHeight: "initial",
+
+      "&:nth-child(2)": {
+        color: "#E07A5F"
+      }
+    }
+  },
+  noOrder: {
+    backgroundColor: "#F9F7ED",
+    color: "#E07A5F",
+    fontFamily: "Microsoft JhengHei",
+    fontSize: 18,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    width: "100%",
+    padding: 10,
+    wordBreak: "normal",
+    marginTop: 20
+  },
+  footerDiv: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    "& > div": {
+      position: "static"
+    }
+  }
+}));
+
+function Orders() {
+  const [restaurant, setrestaurant] = useState(null);
+  const classes = useStyles();
+  const [orders, setOrders] = useState([]);
+  const [isLogIn, setIsLogIn] = useState(true);
+  const history = useHistory();
+
+  const getRestaurant = async () => {
+    let res = await productService.getAll();
+    setrestaurant(res);
+  };
+
+  const getAllOrders = async () => {
+    const user = sessionStorage.getItem("user") !== null ? JSON.parse(sessionStorage.getItem("user")) : {};
+    console.log("user", user);
+    const res = await productService.getAllOrders(user.member);
+    console.log("get all orders res", res);
+    if (res.code === 200) {
+      setIsLogIn(true);
+      if (!restaurant) {
+        getRestaurant();
+      }
+      setOrders(res.orders);
+    } else {
+      setIsLogIn(false);
+      alert("請先登入, 即可查看訂單! 謝謝!");
+      sessionStorage.clear();
+      history.push("/login");
+    }
+  };
+
+  useEffect(() => {
+    console.log("Orders page useEffect is working!!");
+    getAllOrders();
+  }, []);
+
+  const handleDeleteOrder = async order_ID => {
+    console.log("handle delete order order_ID", order_ID);
+    const deleteOrder = window.confirm("請問您確定要刪除此筆訂單嗎?");
+    if (deleteOrder) {
+      const deleteOrder = await productService.deleteOrderDetails(order_ID);
+      console.log("deleteOrder", deleteOrder);
+      if (deleteOrder.code === 400) {
+        alert("您已超過最晚取消預約時間, 無法將您的訂單移除!");
+      } else if (deleteOrder.code === 403) {
+        alert("您已登入超過10分鐘, 系統為確保您的資料安全, 已將您登出!! 請您再次登入進行修改, 謝謝!");
+        sessionStorage.clear();
+        history.push("/login");
+      } else {
+        await getAllOrders();
+        alert(`已將您的訂單編號為 (${order_ID}) 訂單刪除!`);
+      }
+    }
+  };
+
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <Grid item xs={12} className={classes.grid}>
+        <div>
+          <CssBaseline />
+          {isLogIn ? <Header /> : <></>}
+          <main className={classes.content}>
+            {orders.length !== 0 ? (
+              orders.map(item => {
+                const formatDateTime = new Date(item.dateTime);
+                const date = `${formatDateTime.getFullYear()} / ${formatDateTime.getMonth() +
+                  1} / ${formatDateTime.getDate()}`;
+                const minutes =
+                  formatDateTime.getMinutes() > 9 ? formatDateTime.getMinutes() : `0${formatDateTime.getMinutes()}`;
+                const time = `${formatDateTime.getHours()} : ${minutes}`;
+                const createDateTime = new Date(item.create_time);
+                const createDate = `${createDateTime.getFullYear()} / ${createDateTime.getMonth() +
+                  1} / ${createDateTime.getDate()}`;
+                const createMinutes =
+                  createDateTime.getMinutes() > 9 ? createDateTime.getMinutes() : `0${createDateTime.getMinutes()}`;
+                const createTime = `${createDateTime.getHours()} : ${createMinutes}`;
+                return (
+                  <Grid item xs={12} className={classes.orderContent} key={item.create_time}>
+                    <Typography className={classes.orderTitle}>
+                      <span>訂單編號 : </span>
+                      <span>{item._id}</span>
+                    </Typography>
+                    <Typography className={classes.orderTitle}>
+                      <span>成立訂單 : </span>
+                      <span>
+                        {createDate} - {createTime}
+                      </span>
+                    </Typography>
+                    <Card className={classes.card}>
+                      <ul className={classes.orderDetail}>
+                        <li>
+                          日期 : <span>{date}</span>
+                        </li>
+                        <li>
+                          時間 : <span>{time}</span>
+                        </li>
+                        <li>
+                          人數 :{" "}
+                          <span>
+                            {item.adult} 大人 {item.children}小孩
+                          </span>
+                        </li>
+                        <li>
+                          備註 : <span>{item.notes}</span>
+                        </li>
+                      </ul>
+                      <div className={classes.featureBtn}>
+                        <Link to={`/orderdetails?order_ID=${item._id}`}>
+                          <Button>
+                            <CreateRoundedIcon />
+                          </Button>
+                        </Link>
+                        <Button onClick={() => handleDeleteOrder(item._id)}>
+                          <DeleteRoundedIcon />
+                        </Button>
+                      </div>
+                    </Card>
+                  </Grid>
+                );
+              })
+            ) : (
+              <div className={classes.noOrder}>
+                還沒找到喜歡的餐廳嗎?? <br />
+                快回首頁在搜尋一下吧!!
+              </div>
+            )}
+          </main>
+        </div>
+        <div className={classes.footerDiv}>
+          <Footer selectedValue={1} />
+        </div>
+      </Grid>
+    </Container>
+  );
+}
+
+export default Orders;
---
+++ client/src/views/Search.js
@@ -0,0 +1,363 @@
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import productService from "../services/productService";
+import classNames from "classnames";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/button";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Chip from "@material-ui/core/Chip";
+import Footer from "./Footer";
+import Header from "./Header";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    paddingBottom: 30,
+    position: "relative",
+    height: "100vh",
+    boxShadow: "1px 5px 15px 0px #DBDCE1"
+  },
+  container: {
+    position: "relative",
+    height: "100vh",
+    overflow: "scroll",
+    width: 600
+  },
+  grid: {
+    width: "100%",
+    margin: "2px 0",
+    padding: 10,
+    display: "flex",
+    position: "sticky",
+    top: 62,
+    zIndex: 1,
+    backgroundColor: "#fff"
+  },
+  searchBar: {
+    fontSize: 16,
+    color: "#3D405B",
+    border: "1px solid #B8B9C3",
+    borderRadius: 20,
+    width: "100%",
+    padding: "5px 15px 5px",
+    fontFamily: "Microsoft JhengHei",
+
+    "&:focus": {
+      outline: "none"
+    }
+  },
+  searchIcon: {
+    marginLeft: "-40px"
+  },
+  searchBtn: {
+    borderRadius: 50,
+    height: 40,
+    minWidth: 40,
+    padding: 5,
+    "& > span > svg": {
+      color: "#32354B"
+    },
+    "&:hover": {
+      backgroundColor: "#FCF1DB"
+    }
+  },
+  restaurantImage: {
+    width: "75%",
+    height: "100%",
+    margin: "0 auto",
+    "& > img": {
+      width: "100%",
+      borderRadius: 10
+    }
+  },
+  restaurantName: {
+    paddingLeft: 10,
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
+    "-webkit-line-clamp": "2",
+    "-webkit-box-orient": "vertical",
+    fontFamily: "Microsoft JhengHei",
+    fontSize: 17,
+    margin: "0 0 5px",
+    color: "#3D405B"
+  },
+  paperGrid: {
+    position: "relative",
+    marginBottom: 50,
+    "&::after": {
+      content: "''",
+      width: "90%",
+      position: "absolute",
+      bottom: "-35px",
+      border: "0.5px dashed #B8B9C3",
+      left: "50%",
+      transform: "translate(-50%)"
+    }
+  },
+  paperRoot: {
+    position: "absolute",
+    width: "80%",
+    left: "50%",
+    bottom: -15,
+    minHeight: 70,
+    padding: 5,
+    borderRadius: 4,
+    transform: "translate(-50%)"
+  },
+  paperFooter: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0 5px"
+  },
+  chipDiv: {
+    display: "flex",
+    fontFamily: "Microsoft JhengHei",
+    justifyContent: "space-between",
+    padding: 3
+  },
+  chip: {
+    display: "inline-block",
+    backgroundColor: "#E07A5F",
+    color: "#fff",
+    padding: "5px 0px",
+    fontFamily: "Microsoft JhengHei",
+    marginRight: 3
+  },
+  restaurantGrid: {
+    paddingBottom: 70
+  },
+  restaurantMore: {
+    textDecoration: "none",
+    "& > button": {
+      width: "35%",
+      borderRadius: 10,
+      "& > span": {
+        color: "#3D405B"
+      },
+      "&:hover": {
+        backgroundColor: "#FCF1DB"
+      }
+    }
+  },
+  keyWord: {
+    fontFamily: "Microsoft JhengHei",
+    padding: "10px 10px 0px",
+    color: "#3D405B",
+    fontSize: 16
+  },
+  searchResultGrid: {
+    position: "absolute",
+    zIndex: 1,
+    width: "90%",
+    left: 10
+  },
+  saerchListItem: {
+    padding: 10,
+    backgroundColor: "#FEFDFC",
+    border: "1px solid #B8B9C3",
+    borderTop: "none",
+    width: "95%",
+    margin: "0 auto",
+
+    "& > p": {
+      padding: 10,
+      margin: "5px 0",
+      fontFamily: "Microsoft JhengHei",
+      color: "#4E5169",
+      "&:hover": {
+        fontWeight: "bold",
+        backgroundColor: "#F6DAD3",
+        cursor: "pointer"
+      }
+    }
+  },
+  searchNone: {
+    padding: 10,
+    margin: "5px 0",
+    fontFamily: "Microsoft JhengHei",
+    color: "#4E5169",
+    fontWeight: "normal",
+    "&:hover": {
+      fontWeight: "inherit",
+      backgroundColor: "#FEFDFC",
+      cursor: "default"
+    }
+  },
+  urlParams: {
+    color: "#E07A5F",
+    fontWeight: "bold"
+  },
+  searchNoResult: {
+    padding: 10,
+    fontFamily: "Microsoft JhengHei"
+  },
+  footerDiv: {
+    position: "absolute",
+    bottom: 0,
+    width: "inherit",
+    "& > div": {
+      position: "static"
+    }
+  }
+}));
+
+function Search() {
+  const classes = useStyles();
+  const [restaurant, setRestaurant] = useState(null);
+  const [searchKeyWord, setSearchKeyWord] = useState("");
+  const [category, setCategory] = useState();
+  const [searchResult, setSearchResult] = useState([]);
+  const [urlParams, setURLParams] = useState("");
+  const [blur, setBlur] = useState(true);
+  const history = useHistory();
+  const url_params = new URLSearchParams(window.location.search);
+  const keyWord = url_params.get("searchKeyWord");
+  let fetching = function() {};
+  useEffect(() => {
+    if (!restaurant) {
+      getRestaurant();
+      getAllCategory();
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("set search key word!!");
+    setSearchKeyWord(keyWord);
+    setURLParams(keyWord);
+  }, [keyWord]);
+
+  useEffect(() => {
+    console.log("監聽 searchkeyword is working!!");
+    if (searchKeyWord) {
+      console.log(12345, "search key word is changing!", searchKeyWord);
+      fetching = async () => {
+        let res = await productService.searchByKeyWord(searchKeyWord);
+        console.log(123, res, res.restaurants.length);
+        if (res.restaurants.length > 0) {
+          console.log("res 有資料");
+          setSearchResult(res.restaurants);
+          setRestaurant(res.restaurants);
+        }
+      };
+      fetching();
+    }
+  }, [searchKeyWord]);
+
+  const getRestaurant = async () => {
+    setURLParams(keyWord);
+    let res = await productService.searchByKeyWord(keyWord);
+    console.log("111", res.restaurants);
+    setRestaurant(res.restaurants);
+  };
+
+  const getAllCategory = async () => {
+    let res = await productService.getAllCatrgory();
+    setCategory(res.all_category);
+  };
+
+  const handleSubmit = async () => {
+    let res = await productService.searchByKeyWord(searchKeyWord);
+    setURLParams(searchKeyWord);
+    setRestaurant(res.restaurants);
+    setBlur(true);
+    history.push(`/search?searchKeyWord=${searchKeyWord}`);
+  };
+
+  const handleChange = e => {
+    setSearchResult([]);
+    setBlur(false);
+    setSearchKeyWord(e.target.value);
+  };
+
+  const handleClickItem = item => {
+    setSearchKeyWord(item.name);
+    setURLParams(item.name);
+    setRestaurant([item]);
+    setBlur(true);
+  };
+
+  const renderSearchResult = () => {
+    console.log("list item", searchResult);
+    if (searchKeyWord.length > 0 && blur === false) {
+      if (searchResult.length > 0) {
+        const list = searchResult.map((item, idx) => (
+          <p key={idx} onClick={() => handleClickItem(item)}>
+            {item.name}
+          </p>
+        ));
+        return <div className={classes.saerchListItem}>{list}</div>;
+      } else {
+        return (
+          <h4 className={classNames(classes.searchNone, classes.saerchListItem)}>抱歉, 沒有您想要搜尋的餐廳資訊!!</h4>
+        );
+      }
+    }
+  };
+
+  return (
+    <Container maxWidth="sm" className={classes.root}>
+      <Grid item xs={12} className={classes.container}>
+        <Header />
+        <Grid item xs={12} className={classes.grid}>
+          <input
+            type="text"
+            name="searchbar"
+            className={classes.searchBar}
+            value={searchKeyWord}
+            onChange={handleChange}
+            placeholder="search for restaurant"
+          />
+          <div className={classes.searchIcon}>
+            <Button className={classes.searchBtn} onClick={handleSubmit}>
+              <SearchIcon />
+            </Button>
+          </div>
+        </Grid>
+        <Grid item xs={12} className={classes.searchResultGrid}>
+          {renderSearchResult()}
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5" gutterBottom className={classes.keyWord}>
+            為您推薦 <span className={classes.urlParams}>{urlParams}</span> 相關的餐廳 ..
+          </Typography>
+        </Grid>
+        <Grid item xs={12} className={classes.restaurantGrid}>
+          {restaurant && restaurant.length !== 0 ? (
+            restaurant.map(item => (
+              <Grid item xs={12} className={classes.paperGrid} key={item._id}>
+                <div className={classes.restaurantImage}>
+                  <img src={item.image_path.main} alt="" />
+                </div>
+                <Paper className={classes.paperRoot}>
+                  <p className={classes.restaurantName}>{item.name}</p>
+                  <div className={classes.paperFooter}>
+                    <Typography component="div" className={classes.chipDiv}>
+                      <Chip label={item.category.area} className={classes.chip} />
+                      <Chip label={item.category.kind} className={classes.chip} />
+                    </Typography>
+                    <Link to={`/detail?name=${item.name}&_id=${item._id}`} className={classes.restaurantMore}>
+                      <Button onClick={() => productService.getRestaurantDetail(item.name, item._id)}>more</Button>
+                    </Link>
+                  </div>
+                </Paper>
+              </Grid>
+            ))
+          ) : (
+            <p className={classes.searchNoResult}>今天吃點別的吧!!!</p>
+          )}
+        </Grid>
+      </Grid>
+      <div className={classes.footerDiv}>
+        <Footer />
+      </div>
+    </Container>
+  );
+}
+
+export default Search;
