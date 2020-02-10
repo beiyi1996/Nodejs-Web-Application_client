bb0c29a50e23a14a19b8f2401f5538dead978cd0
Winni Huang <usj0326@gmail.com>
2020-02-10 10:26:33 +0800

    add web.config file and express module

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import express from "express";
+
+const server = express();
+const options = {
+  index: "index.html"
+};
+server.use("/", express.static("/home/site/wwwroot", options));
+server.listen(process.env.PORT);
 
 ReactDOM.render(
   <BrowserRouter>
