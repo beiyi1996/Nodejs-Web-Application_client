19449dd69083275de86bb087499ff7990b47aab2
unknown <winniwhuang@liontravel.com>
2020-02-10 14:50:10 +0800

    modified server domain

---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    Serverdomain: "https://winniexpressapp.azurewebsites.net"
+}
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,14 +1,17 @@
+import { Serverdomain } from "../config.js";
+
 export default {
   getAll: async () => {
     console.log("service getAll is working!!!");
-    return await fetch("http://localhost:5000/", { cache: "no-cache" })
+    console.log("Serverdomain", Serverdomain);
+    return await fetch(`${Serverdomain}`, { cache: "no-cache" })
       .then(res => res.json())
       .catch(error => console.error("Error:", error));
   },
   register: async (email, password, name, gender, phone) => {
     const data = { email, password, gender, phone, name };
     console.log("data", data);
-    return await fetch("http://localhost:5000/register", {
+    return await fetch(`${Serverdomain}/register`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -25,7 +28,7 @@ export default {
     const data = { email, password };
     console.log("data", data);
     console.log("JSON.stringify(data)", JSON.stringify(data));
-    return await fetch("http://localhost:5000/login", {
+    return await fetch(`${Serverdomain}/login`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -38,7 +41,7 @@ export default {
   },
   logOut: async logInStatus => {
     const data = { logInStatus };
-    return await fetch("http://localhost:5000/logout", {
+    return await fetch(`${Serverdomain}/logout`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -53,7 +56,7 @@ export default {
     console.log("email", email);
     const data = { email };
     console.log("data", data, JSON.stringify(data));
-    return await fetch(`http://localhost:5000/forgotpassword?email=${email}`, {
+    return await fetch(`${Serverdomain}/forgotpassword?email=${email}`, {
       method: "POST",
       cache: "no-cache",
       headers: new Headers({
@@ -65,7 +68,7 @@ export default {
       .catch(error => console.log("Error:", error));
   },
   modifiedPasswordGET: async token => {
-    return await fetch(`http://localhost:5000/modifiedpassword?token=${token}`, {
+    return await fetch(`${Serverdomain}/modifiedpassword?token=${token}`, {
       cache: "no-cache"
     })
       .then(res => res.json())
@@ -73,7 +76,7 @@ export default {
   },
   modifiedPasswordPOST: async (token, email, password) => {
     const data = { email, password };
-    return await fetch(`http://localhost:5000/modifiedpassword?token=${token}`, {
+    return await fetch(`${Serverdomain}/modifiedpassword?token=${token}`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -85,7 +88,7 @@ export default {
       .catch(error => console.log("Error:", error));
   },
   searchByKeyWord: async searchKeyWord => {
-    return await fetch(`http://localhost:5000/search?searchKeyWord=${searchKeyWord}`, {
+    return await fetch(`${Serverdomain}/search?searchKeyWord=${searchKeyWord}`, {
       method: "POST",
       cache: "no-cache"
     })
@@ -93,14 +96,14 @@ export default {
       .catch(error => console.error("Error:", error));
   },
   getAllCatrgory: async () => {
-    return await fetch("http://localhost:5000/getCategory", {
+    return await fetch(`${Serverdomain}/getCategory`, {
       cache: "no-cache"
     })
       .then(res => res.json())
       .catch(error => console.log("Error:", error));
   },
   getRestaurantDetail: async (name, id) => {
-    return await fetch(`http://localhost:5000/detail?name=${name}&_id=${id}`, { cache: "no-cache" })
+    return await fetch(`${Serverdomain}/detail?name=${name}&_id=${id}`, { cache: "no-cache" })
       .then(res => res.json())
       .catch(error => console.error("Error:", error));
   },
@@ -109,7 +112,7 @@ export default {
     const timeString = `${time}:00`;
     const data = { date, timeString, adult, children, notes, restaurant_name, sessionStorageData };
     console.log("data", data);
-    return await fetch("http://localhost:5000/booking", {
+    return await fetch(`${Serverdomain}/booking`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -121,7 +124,7 @@ export default {
       .catch(error => console.log("Error:", error));
   },
   getAllOrders: async userName => {
-    return await fetch(`http://localhost:5000/orders?name=${userName}`, {
+    return await fetch(`${Serverdomain}/orders?name=${userName}`, {
       cache: "no-cache"
     })
       .then(res => res.json())
@@ -129,7 +132,7 @@ export default {
   },
   getOrderDetails: async order_id => {
     console.log("service order_id", order_id);
-    return await fetch(`http://localhost:5000/orderdetails?order_ID=${order_id}`, {
+    return await fetch(`${Serverdomain}/orderdetails?order_ID=${order_id}`, {
       cache: "no-cache"
     })
       .then(res => res.json())
@@ -139,7 +142,7 @@ export default {
     const timeString = `${time}:00`;
     const data = { clickDate, timeString, adult, children, notes };
     console.log("save change order details data", data);
-    return await fetch(`http://localhost:5000/orderdetails/save?order_ID=${order_id}`, {
+    return await fetch(`${Serverdomain}/orderdetails/save?order_ID=${order_id}`, {
       method: "POST",
       body: JSON.stringify(data),
       cache: "no-cache",
@@ -152,21 +155,21 @@ export default {
   },
   deleteOrderDetails: async order_id => {
     console.log(2, "delete order details service is working!!!");
-    return await fetch(`http://localhost:5000/orderdetails/delete?order_ID=${order_id}`, {
+    return await fetch(`${Serverdomain}/orderdetails/delete?order_ID=${order_id}`, {
       cache: "no-cache"
     })
       .then(res => res.json())
       .catch(error => console.log("Error:", error));
   },
   checkLogInStatus: async name => {
-    return await fetch(`http://localhost:5000/checkLogInStatus?name=${name}`, {
+    return await fetch(`${Serverdomain}/checkLogInStatus?name=${name}`, {
       cache: "no-cache"
     })
       .then(res => console.log("check log in status", res))
       .catch(error => console.log("check log in status Error", error));
   },
   getRestaurantName: async _id => {
-    return await fetch(`http://localhost:5000/restaurantName?_id=${_id}`, {
+    return await fetch(`${Serverdomain}/restaurantName?_id=${_id}`, {
       cache: "no-cache"
     })
       .then(res => res.json())
