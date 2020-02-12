8e4156dc008e12a55e73f02615b60a2691eb96bd
unknown <winniwhuang@liontravel.com>
2020-02-12 11:30:13 +0800

    test server.js working

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.listen(process.env.PORT || 5000);
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+// console.log("react server is working??");
+// app.listen(process.env.PORT || 5000);
